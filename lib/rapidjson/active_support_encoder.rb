e094c6dbb124a9290f6f7ff0e7ba30a9df77f2eb
John Hawthorn <john@hawthorn.email>
2023-06-26 18:28:35 -0700

    Add RapidJSON::ActiveSupportEncoder

---
+++ lib/rapidjson/active_support_encoder.rb
@@ -0,0 +1,26 @@
+module RapidJSON
+  class ActiveSupportEncoder
+    def initialize(options = nil)
+      @options = options
+      @coder = RapidJSON::Coder.new do |value, is_key|
+        if is_key
+          value.to_s
+        else
+          value.as_json
+        end
+      end
+    end
+
+    # Encode the given object into a JSON string
+    def encode(value)
+      if @options && !@options.empty?
+        value = value.as_json(@options.dup)
+      end
+      json = @coder.dump(value)
+      if ActiveSupport::JSON::Encoding.escape_html_entities_in_json
+        json = RapidJSON.json_escape(json)
+      end
+      json
+    end
+  end
+end
